9065e87d74f1b1a81ca82c69c1c2051caaf28670
albert <zzhen0v0@gmail.com>
2024-03-14 22:32:07 +0900

    initialize gulimail

---
+++ gulimail-order/src/main/java/com/github/zzhen0v0/gulimail/order/GulimailOrderApplication.java
@@ -0,0 +1,13 @@
+package com.github.zzhen0v0.gulimail.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimailOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimailOrderApplication.class, args);
+    }
+
+}
